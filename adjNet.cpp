64e48bf7c4c5a9a0427a8e3b1cc1fd824b171a12
‘Maverick <1097249217@qq.com>
2019-12-11 10:36:08 +0800

    ✌

--- adjNet.cpp
+++ adjNet.cpp
@@ -1,19 +1,34 @@
 #include "adjNet.h"
 //构造函数:根据老师所给图生成相应网
 adjNet::adjNet(){
-    for(int i=0;i<9;i++){
-        adjlist.push_back(adjNode(i));
-    }
+    adjlist.push_back(adjNode());
     //adjlist[0].nextedgeNode=new edgeNode(2,6,);
 }
-void adjNet::InsertEdge(int v1,int v2,int w){
+int adjNet::InsertEdge(int v1,int v2,int w){
+    int num=adjlist.size();
+    if(v1>num||v2>num){
+        return -1;
+    }
     edgeNode *n=adjlist[v1-1].nextedgeNode;
     while(n!=NULL){
         n=n->nextedgeNode;
     }//到末尾退出
     n->nextedgeNode=new edgeNode(v2,w);//添置新节点
+    return 1;
 }
 
+//添加顶点vex
+void adjNet::InsetVex(){
+    int n=adjlist.size();
+    adjNode *a1= new adjNode(n);
+    adjlist.push_back((*a1));
+}
+//获得结点数
 int adjNet::GetVexNum(){
     return adjlist.size();
 } 
+
+//返回第一个邻接结点
+int adjNet::FirstAdjVex(int v1){
+    return adjlist[v1].nextedgeNode->adjnum;
+}
--- adjNet.h
+++ adjNet.h
@@ -21,7 +21,10 @@ struct edgeNode
 struct adjNode{
     int vex;//表头序号，代表第几个节点
     edgeNode* nextedgeNode;//指向下一个表边节点
-    adjNode();//无参
+    adjNode(){
+        vex=0;
+        nextedgeNode=NULL;
+    };//无参
     adjNode(int v,edgeNode* next=NULL){
         vex=v;nextedgeNode=next;
     }
@@ -41,7 +44,8 @@ class adjNet{
         int FirstAdjVex(int v1);//返回第一个邻接点
         int NextAdjVex(int v1,int v2);//返回v1相对于v2的邻接点
         int GetWeight(int v1,int v2);//返回两点间的权值
-        void InsertEdge(int v1,int v2,int w);//在末尾插入一条边
+        int InsertEdge(int v1,int v2,int w);//在末尾插入一条边
+        void InsetVex();//添加一个顶点,仅能从末尾追加
 };
 
 
