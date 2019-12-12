f6432d431c7f7d15e6eb1fb7ef175e11b343da9d
Maverick <1097249217@qq.com>
2019-12-12 13:14:46 +0800

    😭

--- criticalpath.h
+++ criticalpath.h
@@ -3,8 +3,8 @@
 
 #include "adjNet.h"
 
-void statIndegree(adjNet &net,int *indegree);//统计各个节点的入度
+void statIndegree(adjNet& net,int *indegree);//统计各个节点的入度
 
-int Criticalpath(adjNet &net);//输出关键活动，成功返回1，失败返回-1
+int Criticalpath(adjNet& net);//输出关键活动，成功返回1，失败返回-1
 
 #endif
